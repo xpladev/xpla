3fa743277ebd404b8171382e00d7f695abf9548a
JoowonYun <joowon@delightlabs.io>
2023-03-13 17:18:37 +0900

    fix: typo

--- x/xatp/types/genesis.pb.go
+++ x/xatp/types/genesis.pb.go
@@ -23,7 +23,7 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
-// GenesisState defines the xtps module's genesis state.
+// GenesisState defines the xatp module's genesis state.
 type GenesisState struct {
 	// params defines all the paramaters of the module.
 	Params Params `protobuf:"bytes,1,opt,name=params,proto3" json:"params" yaml:"params"`
