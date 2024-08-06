a425ae6a370cc9618f1cbd8bfd47fda2b2a200b4
JoowonYun <joowon@delightlabs.io>
2024-08-06 17:52:24 +0900

    fix: rollback refunded gas

--- app/upgrades/v1_6/upgrades.go
+++ app/upgrades/v1_6/upgrades.go
@@ -1,12 +1,17 @@
 package v1_6
 
 import (
+	"math/big"
+
 	"cosmossdk.io/errors"
+	sdkmath "cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 
 	evmtypes "github.com/xpladev/ethermint/x/evm/types"
 	"github.com/xpladev/xpla/app/keepers"
@@ -40,6 +45,24 @@ func CreateUpgradeHandler(
 
 		if res.Failed() {
 			return nil, errors.ErrPanic.Wrap(res.VmError)
+		}
+
+		// Gas refunded rollback without use
+		tx := msg.AsTransaction()
+
+		signer := ethtypes.NewLondonSigner(keepers.EvmKeeper.ChainID())
+		coreMsg, err := msg.AsMessage(signer, keepers.FeeMarketKeeper.CalculateBaseFee(ctx))
+		if err != nil {
+			return nil, err
+		}
+
+		refundedGas := msg.GetGas() - res.GasUsed
+		refundAmount := new(big.Int).Mul(new(big.Int).SetUint64(refundedGas), tx.GasPrice())
+		evmDenom := keepers.EvmKeeper.GetParams(ctx).EvmDenom
+
+		err = keepers.BankKeeper.SendCoinsFromAccountToModule(ctx, coreMsg.From().Bytes(), authtypes.FeeCollectorName, sdk.NewCoins(sdk.NewCoin(evmDenom, sdkmath.NewIntFromBigInt(refundAmount))))
+		if err != nil {
+			return nil, err
 		}
 
 		return mm.RunMigrations(ctx, configurator, fromVM)
