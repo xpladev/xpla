47891546394fa044371beb686cb814380daea0f4
sooyoung <sooyoung@delightlabs.io>
2024-10-24 20:53:10 +0900

    fix tests/integration params

--- tests/integration/testutil/common.go
+++ tests/integration/testutil/common.go
@@ -2,14 +2,14 @@ package testutil
 
 import (
 	"testing"
-	//"time"
+	"time"
 
 	//"github.com/stretchr/testify/require"
 
 	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
 
 	"cosmossdk.io/log"
-	//sdkmath "cosmossdk.io/math"
+	sdkmath "cosmossdk.io/math"
 	//simparams "cosmossdk.io/simapp/params"
 	//"cosmossdk.io/store"
 	//storemetrics "cosmossdk.io/store/metrics"
@@ -17,10 +17,12 @@ import (
 
 	dbm "github.com/cosmos/cosmos-db"
 	"github.com/cosmos/cosmos-sdk/codec"
+
 	//codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	//"github.com/cosmos/cosmos-sdk/std"
 	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	//"github.com/cosmos/cosmos-sdk/types/module"
 	//"github.com/cosmos/cosmos-sdk/x/auth"
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
@@ -31,7 +33,7 @@ import (
 	//banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	//distr "github.com/cosmos/cosmos-sdk/x/distribution"
 	distrkeeper "github.com/cosmos/cosmos-sdk/x/distribution/keeper"
-	//distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 	//govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	//"github.com/cosmos/cosmos-sdk/x/mint"
 	//mintkeeper "github.com/cosmos/cosmos-sdk/x/mint/keeper"
@@ -40,17 +42,17 @@ import (
 	//paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	//"github.com/cosmos/cosmos-sdk/x/slashing"
 	slashingkeeper "github.com/cosmos/cosmos-sdk/x/slashing/keeper"
-	//slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
+	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
 	//"github.com/cosmos/cosmos-sdk/x/staking"
 	stakingtestutil "github.com/cosmos/cosmos-sdk/x/staking/testutil"
-	//stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
 	//etherminttypes "github.com/xpladev/ethermint/types"
 
 	xplaApp "github.com/xpladev/xpla/app"
 	//"github.com/xpladev/xpla/x/reward"
 	rewardkeeper "github.com/xpladev/xpla/x/reward/keeper"
-	//rewardtypes "github.com/xpladev/xpla/x/reward/types"
+	rewardtypes "github.com/xpladev/xpla/x/reward/types"
 	stakingkeeper "github.com/xpladev/xpla/x/staking/keeper"
 	//"github.com/xpladev/xpla/x/volunteer"
 	volunteerkeeper "github.com/xpladev/xpla/x/volunteer/keeper"
@@ -98,9 +100,35 @@ func CreateTestInput(t *testing.T) TestInput {
 		xplaApp.EmptyAppOptions{},
 		xplaApp.EmptyWasmOptions)
 
-	ctx := app.BaseApp.NewUncachedContext(true, tmproto.Header{})
+	ctx := app.BaseApp.NewUncachedContext(true, tmproto.Header{Time: time.Now().UTC()})
+
+	// Params setting
+	keepers := app.AppKeepers
+
+	stakingParams := stakingtypes.DefaultParams()
+	stakingParams.BondDenom = sdk.DefaultBondDenom
+	keepers.StakingKeeper.SetParams(ctx, stakingParams)
+
+	keepers.SlashingKeeper.SetParams(ctx, slashingtypes.DefaultParams())
+
+	keepers.DistrKeeper.FeePool.Set(ctx, distrtypes.InitialFeePool())
+	distrParams := distrtypes.DefaultParams()
+	distrParams.CommunityTax = sdkmath.LegacyZeroDec()
+	keepers.DistrKeeper.Params.Set(ctx, distrParams)
+
+	keepers.MintKeeper.Params.Set(ctx, minttypes.DefaultParams())
+
+	rewardParams := rewardtypes.Params{
+		FeePoolRate:             sdkmath.LegacyNewDecWithPrec(20, 2),
+		CommunityPoolRate:       sdkmath.LegacyNewDecWithPrec(79, 2),
+		ReserveRate:             sdkmath.LegacyNewDecWithPrec(1, 2),
+		ReserveAccount:          sdk.AccAddress(Pks[ReserveIndex].Address()).String(),
+		RewardDistributeAccount: sdk.AccAddress(Pks[ValidatorSettlementIndex].Address()).String(),
+	}
+	keepers.RewardKeeper.SetParams(ctx, rewardParams)
 
 	sh := stakingtestutil.NewHelper(t, ctx, app.AppKeepers.StakingKeeper.Keeper)
+	app.ModuleBasics.RegisterInterfaces(app.InterfaceRegistry())
 
 	return TestInput{ctx, app.LegacyAmino(), app.AppKeepers.AccountKeeper.AccountKeeper, app.AppKeepers.BankKeeper, app.AppKeepers.RewardKeeper, app.AppKeepers.StakingKeeper, app.AppKeepers.SlashingKeeper, app.AppKeepers.DistrKeeper, app.AppKeepers.VolunteerKeeper, sh}
 }
