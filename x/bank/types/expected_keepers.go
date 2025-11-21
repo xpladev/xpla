69bceffe0cd207cb7e1e69f07948314837255b27
JoowonYun <joowon@delightlabs.io>
2025-11-21 15:58:47 +0900

    refactor: use evm module's CallEVM

--- x/bank/types/expected_keepers.go
+++ x/bank/types/expected_keepers.go
@@ -2,8 +2,10 @@ package types
 
 import (
 	"context"
+	"math/big"
 
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	common "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/tracing"
@@ -17,6 +19,15 @@ type EvmKeeper interface {
 	ApplyMessage(ctx sdk.Context, msg core.Message, tracer *tracing.Hooks, commit bool, internal bool) (*evmtypes.MsgEthereumTxResponse, error)
 	EstimateGas(c context.Context, req *evmtypes.EthCallRequest) (*evmtypes.EstimateGasResponse, error)
 	GetNonce(ctx sdk.Context, addr common.Address) uint64
+	CallEVM(
+		ctx sdk.Context,
+		abi abi.ABI,
+		from, contract common.Address,
+		commit bool,
+		gasCap *big.Int,
+		method string,
+		args ...interface{},
+	) (*evmtypes.MsgEthereumTxResponse, error)
 }
 
 type WasmMsgServer interface {
