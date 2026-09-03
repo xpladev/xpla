c12960592ec83caf82a80f638c45739ef2566829
nowooj <joowon@delightlabs.io>
2026-09-03 15:11:32 +0900

    fix(burn): paginate ongoing proposal queries

--- x/burn/keeper/grpc_query.go
+++ x/burn/keeper/grpc_query.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/xpladev/xpla/x/burn/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -20,10 +21,19 @@ func (k Querier) OngoingProposals(c context.Context, req *types.QueryOngoingProp
 		return nil, status.Error(codes.InvalidArgument, "empty request")
 	}
 
-	ctx := sdk.UnwrapSDKContext(c)
-	proposals := k.GetAllOngoingBurnProposals(ctx)
+	proposals, pageRes, err := query.CollectionPaginate(
+		c,
+		k.OngoingBurnProposals,
+		req.Pagination,
+		func(_ uint64, proposal types.BurnProposal) (types.BurnProposal, error) {
+			return proposal, nil
+		},
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "paginate: %v", err)
+	}
 
-	return &types.QueryOngoingProposalsResponse{Proposals: proposals}, nil
+	return &types.QueryOngoingProposalsResponse{Proposals: proposals, Pagination: pageRes}, nil
 }
 
 func (k Querier) OngoingProposal(c context.Context, req *types.QueryOngoingProposalRequest) (*types.QueryOngoingProposalResponse, error) {
